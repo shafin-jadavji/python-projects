6913c5c67aee2138f6e7e2db56af518359a7ab0a
Shafin Jadavji <shafinj@gmail.com>
2024-09-15 20:07:54 -0700

    refactor: create constants and DRY

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -19,8 +19,12 @@
 
 import random
 
-choices = ("r", "p", "s")
-emojis ={ "r": "🪨", "p": "📃", "s": "✂️"}
+ROCK = "r"
+PAPER = "p"
+SCISSORS = "s"
+
+emojis ={ ROCK: "🪨", PAPER: "📃", SCISSORS: "✂️"}
+choices = (tuple(emojis.keys()))
 
 def get_user_choice():
     """
@@ -73,9 +77,9 @@ def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
         print("Tie!")
     elif (
-        (user_choice == "r" and computer_choice == "s") or
-        (user_choice == "p" and computer_choice == "r") or
-        (user_choice == "s" and computer_choice == "p")
+        (user_choice == ROCK and computer_choice == SCISSORS) or
+        (user_choice == PAPER and computer_choice == ROCK) or
+        (user_choice == SCISSORS and computer_choice == PAPER)
     ):
         print("You win!")
     else:
